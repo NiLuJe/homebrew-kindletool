a44c41c4a57f370c54eef4d5b4c9d52cc42cf6b6
NiLuJe <ninuje@gmail.com>
2013-12-21 22:22:08 +0100

    Let's check...

--- Formula/kindletool.rb
+++ Formula/kindletool.rb
@@ -8,20 +8,22 @@ class Kindletool < Formula
   head 'https://github.com/NiLuJe/KindleTool.git'
 
   depends_on 'pkg-config' => :build
-  depends_on 'NiLuJe/kindletool/libarchive'
+  #depends_on 'NiLuJe/kindletool/libarchive'
+  depends_on 'libarchive'
   depends_on 'NiLuJe/kindletool/nettle'
 
   def install
     # Superenv doesn't append keg_only packages to the pkg-config searchpath... Note the null separator, pkg-config deals very badly with spaces ;p!
-    ENV.append 'PKG_CONFIG_PATH', ":#{Formula.factory('libarchive').opt_prefix}/lib/pkgconfig", ''
+    #ENV.append 'PKG_CONFIG_PATH', ":#{Formula.factory('libarchive').opt_prefix}/lib/pkgconfig", ''
     # NOTE: Oops. That very well may be true, but the main issue here is that libarchive doesn't even *build* a pkg-config file when build with CMake...
     
     # Superenv kills *FLAGS from the env, which breaks our Makefile...
     ENV['CFLAGS'] = '-O2 ${HOMEBREW_OPTFLAGS}'
     # Those need to be set for our Makefile's logic.
-    ENV['CPPFLAGS'] = ''
+    ENV['CPPFLAGS'] = ' '
+    ENV['LDFLAGS'] = ' '
 
-    system 'make', 'LDFLAGS='
-    system 'make', 'install', "DESTDIR=#{prefix}", 'PREFIX=/.', 'LDFLAGS='
+    system 'make'
+    system 'make', 'install', "DESTDIR=#{prefix}", 'PREFIX=/.'
   end
 end
