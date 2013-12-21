d487356a96ac2523bd8f71fab628ba7ae83e15e8
NiLuJe <ninuje@gmail.com>
2013-12-21 21:20:00 +0100

    Ooops.
    My bad.

--- Formula/kindletool.rb
+++ Formula/kindletool.rb
@@ -14,6 +14,8 @@ class Kindletool < Formula
   def install
     # Superenv doesn't append keg_only packages to the pkg-config searchpath... Note the null separator, pkg-config deals very badly with spaces ;p!
     ENV.append 'PKG_CONFIG_PATH', ":#{Formula.factory('libarchive').opt_prefix}/lib/pkgconfig", ''
+    # NOTE: Oops. That very well may be true, but the main issue here is that libarchive doesn't even *build* a pkg-config file when build with CMake...
+    
     # Superenv kills *FLAGS from the env, which breaks our Makefile...
     ENV['CFLAGS'] = '-O2 ${HOMEBREW_OPTFLAGS}'
     # Those need to be set for our Makefile's logic.
