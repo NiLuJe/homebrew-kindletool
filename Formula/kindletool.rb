3a52bb7a2f2b4bb32fd5386842b0b94969cad019
NiLuJe <ninuje@gmail.com>
2013-12-21 22:33:17 +0100

    Fix kindletool
    Yep, it works much better if we actually have a pkgconfig file xD

--- Formula/kindletool.rb
+++ Formula/kindletool.rb
@@ -8,16 +8,11 @@ class Kindletool < Formula
   head 'https://github.com/NiLuJe/KindleTool.git'
 
   depends_on 'pkg-config' => :build
-  #depends_on 'NiLuJe/kindletool/libarchive'
-  depends_on 'libarchive'
+  depends_on 'NiLuJe/kindletool/libarchive'
   depends_on 'NiLuJe/kindletool/nettle'
 
   def install
-    # Superenv doesn't append keg_only packages to the pkg-config searchpath... Note the null separator, pkg-config deals very badly with spaces ;p!
-    #ENV.append 'PKG_CONFIG_PATH', ":#{Formula.factory('libarchive').opt_prefix}/lib/pkgconfig", ''
-    # NOTE: Oops. That very well may be true, but the main issue here is that libarchive doesn't even *build* a pkg-config file when build with CMake...
-    
-    # Superenv kills *FLAGS from the env, which breaks our Makefile...
+    # Superenv kills *FLAGS from the env, which confuses our Makefile...
     ENV['CFLAGS'] = '-O2 ${HOMEBREW_OPTFLAGS}'
     # Those need to be set for our Makefile's logic.
     ENV['CPPFLAGS'] = ' '
