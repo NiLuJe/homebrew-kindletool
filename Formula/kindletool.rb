f6b2d59fc881c34e42a0447709dde758a1c9c594
NiLuJe <ninuje@gmail.com>
2013-12-21 19:23:09 +0100

    Kill trailing whitespace

--- Formula/kindletool.rb
+++ Formula/kindletool.rb
@@ -4,10 +4,10 @@ class Kindletool < Formula
   homepage 'https://github.com/NiLuJe/KindleTool'
   url 'https://github.com/NiLuJe/KindleTool/archive/v1.5.9.tar.gz'
   sha1 'aa8167c0e239456ab3690d6ad98804908f92f1f8'
-  
+
   head 'https://github.com/NiLuJe/KindleTool.git'
 
-  depends_on 'pkgconfig' => :build
+  depends_on 'pkg-config' => :build
   depends_on 'NiLuJe/kindletool/libarchive'
   depends_on 'NiLuJe/kindletool/nettle'
 
--- Formula/nettle.rb
+++ Formula/nettle.rb
@@ -4,7 +4,7 @@ class Nettle < Formula
   homepage 'http://www.lysator.liu.se/~nisse/nettle/'
   url 'http://www.lysator.liu.se/~nisse/archive/nettle-2.7.1.tar.gz'
   sha1 'e7477df5f66e650c4c4738ec8e01c2efdb5d1211'
-  
+
   head 'git://git.lysator.liu.se/nettle/nettle.git'
 
   depends_on 'gmp'
@@ -12,7 +12,7 @@ class Nettle < Formula
   def install
     # Don't add -ggdb3 to the CFLAGS!
     inreplace 'configure', 'CFLAGS="$CFLAGS -ggdb3"', 'true'
-    
+
     system "./configure", "--prefix=#{prefix}",
                           "--enable-shared",
                           "--enable-public-key",
