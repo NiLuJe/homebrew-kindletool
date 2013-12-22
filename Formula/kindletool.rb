28c480f28d12f5d2aa5928468c4ba67072b4e1fb
NiLuJe <ninuje@gmail.com>
2013-12-22 22:36:06 +0100

    Bump kindletool stable to 1.6.0

--- Formula/kindletool.rb
+++ Formula/kindletool.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Kindletool < Formula
   homepage 'https://github.com/NiLuJe/KindleTool'
-  url 'https://github.com/NiLuJe/KindleTool/archive/v1.5.9.tar.gz'
-  sha1 'aa8167c0e239456ab3690d6ad98804908f92f1f8'
+  url 'https://github.com/NiLuJe/KindleTool/archive/v1.6.0.tar.gz'
+  sha1 'fc305c91ca1d42db85cb60475dc4acd42d0122db'
 
   head 'https://github.com/NiLuJe/KindleTool.git'
 
