f023c903c841e538a8ba11b8a942f2b32d5a87a1
NiLuJe <ninuje@gmail.com>
2014-04-10 02:42:18 +0200

    Hope?

--- Formula/kindletool.rb
+++ Formula/kindletool.rb
@@ -8,7 +8,6 @@ class Kindletool < Formula
   head 'https://github.com/NiLuJe/KindleTool.git', :shallow => false
 
   depends_on 'pkg-config' => :build
-  depends_on 'NiLuJe/kindletool/libarchive'
   # FIXME: Call me when homebrew stops getting its panties in a bunch...
   # If we uncomment this perfectly fine and proper dep, we end up with a
   # (very helpful...) 'Error: Operation already in progress for nettle'...
@@ -18,8 +17,9 @@ class Kindletool < Formula
   # because the workaround of depending on libarchive --with-nettle is ugly and wrong,
   # and also annoying, because we have to create a bogus with-nettle option in libarchive
   # to satisfy this conditional, since :recommended only create a without-* option...
-  # Levae this in, commented, so that brew audit complains about it if I ever forget about it...
-  #depends_on 'NiLuJe/kindletool/nettle'
+  # Leave this in, commented, so that brew audit complains about it if I ever forget about it...
+  depends_on 'NiLuJe/kindletool/nettle'
+  depends_on 'NiLuJe/kindletool/libarchive'
 
   def install
     # NOTE: Leave my damn warnings alone! (noop with superenv)
