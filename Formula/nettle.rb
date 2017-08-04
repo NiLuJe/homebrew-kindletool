2a771cff14326e235ef38bdb4bc21b3e1135a410
NiLuJe <ninuje@gmail.com>
2017-08-03 23:10:48 -0700

    Resync neetle Formula

--- Formula/nettle.rb
+++ Formula/nettle.rb
@@ -1,17 +1,17 @@
 class Nettle < Formula
   desc "Low-level cryptographic library"
   homepage "http://www.lysator.liu.se/~nisse/nettle/"
-  url "http://ftpmirror.gnu.org/nettle/nettle-3.2.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/nettle/nettle-3.2.tar.gz"
-  sha256 "ea4283def236413edab5a4cf9cf32adf540c8df1b9b67641cfc2302fca849d97"
+  url "https://ftp.gnu.org/gnu/nettle/nettle-3.3.tar.gz"
+  mirror "https://ftpmirror.gnu.org/nettle/nettle-3.3.tar.gz"
+  sha256 "46942627d5d0ca11720fec18d81fc38f7ef837ea4197c1f630e71ce0d470b11e"
 
   head "https://git.lysator.liu.se/nettle/nettle.git"
 
   bottle do
     cellar :any
-    sha256 "149db1957c10656b05dd887d70ec699dc5e3776790fd2208a37b3a6fafa47f66" => :el_capitan
-    sha256 "9d26a23ec1699a09d84dba677eba18944f9c7480e2061b36bb4c8ec2bca13a9e" => :yosemite
-    sha256 "f86d2cf88360585545fb7309c8d631717801d90ecdfd9fdaf094aff32f4829f5" => :mavericks
+    sha256 "c111158ee75fde15a6b5a0417416f62358dfb0d06fcab0311b1d9d4849df5fa2" => :sierra
+    sha256 "b23a2c67db98f807d240fc581ee87b4eb4284b1eabda8d38e09b8723eb6b4b62" => :el_capitan
+    sha256 "fa2a4eb958c0f9a1ec019264e31c0c98a08c9f204f146458ca62a43e5c3029a0" => :yosemite
   end
 
   head do
@@ -20,7 +20,7 @@ class Nettle < Formula
   depends_on "gmp"
 
   def install
-    # OS X doesn't use .so libs. Emailed upstream 04/02/2016.
+    # macOS doesn't use .so libs. Emailed upstream 04/02/2016.
     inreplace "testsuite/dlopen-test.c", "libnettle.so", "libnettle.dylib"
 
     system "./.bootstrap" if build.head?
