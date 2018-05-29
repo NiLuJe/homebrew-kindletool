d4d102f6be64fef3cf9216fc2a67822f656e90c2
NiLuJe <ninuje@gmail.com>
2018-05-29 20:55:37 +0200

    Strip legacy stuff
    Appears to work right now...

--- Formula/kindletool.rb
+++ Formula/kindletool.rb
@@ -7,23 +7,11 @@ class Kindletool < Formula
   head "https://github.com/NiLuJe/KindleTool.git", :shallow => false
 
   depends_on "pkg-config" => :build
-  # FIXME: Call me when homebrew stops getting its panties in a bunch...
-  # If we uncomment this perfectly fine and proper dep, we end up with a
-  # (very helpful...) 'Error: Operation already in progress for nettle'...
-  # Bets guess: homebrew doesn't like the fact that libarchive already depends
-  # on nettle through a :recommended...
-  # Current workaround: just comment the dep, and hope users follow the README,
-  # because the workaround of depending on libarchive --with-nettle is ugly and wrong,
-  # and also annoying, because we have to create a bogus with-nettle option in libarchive
-  # to satisfy this conditional, since :recommended only create a without-* option...
-  # Leave this in, commented, so that brew audit complains about it if I ever forget about it...
+
   depends_on "NiLuJe/kindletool/nettle"
   depends_on "NiLuJe/kindletool/libarchive"
 
   def install
-    # NOTE: Leave my damn warnings alone! (noop with superenv)
-    #ENV.enable_warnings
-
     # Make sure the buildsystem will be able to generate a proper version tag
     ENV["GIT_DIR"] = cached_download/".git" if build.head?
 
